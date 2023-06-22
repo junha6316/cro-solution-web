b2026cf22ee52a8a9defb86b326c2de146361bee
junha6316 <junha6316@gmail.com>
2023-06-22 21:42:11 +0900

    수정: formData로 수정

--- src/app/api/cafe24/getAccessToken/route.ts
+++ src/app/api/cafe24/getAccessToken/route.ts
@@ -10,6 +10,12 @@ export async function POST(req: NextRequest) {
     const credentials = `${clientId}:${clientSecret}`;
     const base64Credentials = Buffer.from(credentials).toString("base64");
 
+    const formData = new URLSearchParams();
+
+    formData.append("grant_type", "authorization_code");
+    formData.append("code", authCode);
+    formData.append("redirect_uri", redirectUri);
+
     const response = await fetch(
       `https://${mallId}.cafe24api.com/api/v2/oauth/token`,
       {
@@ -18,11 +24,7 @@ export async function POST(req: NextRequest) {
           Authorization: `Basic ${base64Credentials}`,
           "Content-Type": "application/x-www-form-urlencoded",
         },
-        body: JSON.stringify({
-          grant_type: "authorization_code",
-          code: authCode,
-          redirect_uri: redirectUri,
-        }),
+        body: formData.toString(),
       }
     );
 

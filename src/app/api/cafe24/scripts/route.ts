731bb7c5c399c2c0e82c899fc3335c558e8aaf55
junha6316 <junha6316@gmail.com>
2023-06-25 17:03:01 +0900

    추가: 스크립트 태그 API 추가

---
+++ src/app/api/cafe24/scripts/route.ts
@@ -0,0 +1,31 @@
+import Cookies from "js-cookie";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { shop_no, accessToken, src } = await req.json();
+    const response = await fetch(
+      `https://${mallId}.cafe24api.com/api/v2/admin/scripttags`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+          "X-Cafe24-Api-Version": " 2023-06-01",
+        },
+        body: JSON.stringify({
+          shop_no,
+          src,
+          display_location: "all",
+        }),
+      }
+    );
+    const data = await response.json();
+    return NextResponse.json({
+      ...data,
+    });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({});
+  }
+}
